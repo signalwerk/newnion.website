ccf93afd45e6840766ce81a42a2fa6a2d3ccaddf
signalwerk <sh@signalwerk.ch>
2023-12-26 02:13:37 +0100

    ADD: new build system

---
+++ src/config.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+function page(node, { Helmet, processor }) {
+  if (!node) return null;
+  return (
+    <>
+      <Helmet>
+        <title>{node.title}</title>
+        <meta name="description" content={node.description} />
+      </Helmet>
+      <div class="background">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <div className="content">
+        <h4>
+          <span className="logo">Newnion</span>
+        </h4>
+        <div className={`node-page ${node.class || ""}`}>
+          <>{node.children && processor.run(node.children)}</>
+        </div>
+      </div>
+    </>
+  );
+}
+
+const config = {
+  types: {
+    page: page,
+  },
+  // for the admin interface
+  // admin: {
+  //   // for init the admin interface
+  //   init: ({ CMS }) => {
+  //     CMS.registerPreviewStyle(url);
+  //   },
+  // },
+};
+export default config;
