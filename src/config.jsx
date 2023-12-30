92c711b32613de045c37c464eecb2e6d1d1162c6
signalwerk <sh@signalwerk.ch>
2023-12-30 03:00:39 +0100

    ADD: main-menu handling

--- src/config.jsx
+++ src/config.jsx
@@ -1,7 +1,67 @@
 import React from "react";
 
+// check abstraction
+const isActive = (item, path) => item.path === path;
+
+// set an 'active' key on each object if
+// a child is active
+const setActive = (menu, path) => {
+  // the returned menu
+  let newMenu = {
+    ...menu,
+  };
+  // is this menu active
+  let subActive = false;
+
+  // children check
+  if (menu.children) {
+    newMenu.children = [];
+    menu.children.forEach((item) => {
+      let { active, menu } = setActive(item, path);
+      if (active) {
+        subActive = true;
+      }
+      newMenu.children.push(menu);
+    });
+  }
+
+  // if one of the children is active or the item itself
+  newMenu.active = subActive || isActive(menu, path);
+
+  // return
+  return {
+    active: newMenu.active,
+    menu: newMenu,
+  };
+};
+
+function menu(items, { level } = { level: 0 }) {
+  if (!items) return null;
+  return (
+    <ul className={`menu menu--level-${level}`}>
+      {items.map((item) => {
+        return (
+          <li
+            className={`menu__item menu__item--${
+              item.active ? "active" : "not-active"
+            }`}
+          >
+            <a href={item.path} className="menu__item-link">
+              <span className="menu__item-inner">{item.label}</span>
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
 function page(node, { Helmet, processor }) {
   if (!node) return null;
+
+  const menuData =
+    setActive({ children: node?.menus?.main?.items }, node.path || "/") || null;
+
   return (
     <>
       <Helmet>
@@ -37,6 +97,9 @@ function page(node, { Helmet, processor }) {
             <div className="logo__inner">Newnion</div>
           </div>
         </div>
+        {menuData?.menu?.children && (
+          <div className="header__menu">{menu(menuData?.menu?.children)}</div>
+        )}
       </div>
 
       <div className="content">
